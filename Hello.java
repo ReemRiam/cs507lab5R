25eda7df1fb8767b10166a2a434c1a65ee257a70
wuyyurub543 <wuyyurub543@strose.edu>
2015-03-05 02:00:35 -0500

    adding changes

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Reem, Riam,Bindu");
+	System.out.println("Software Engineering Lab5");
+        System.out.println("Reem, Riam, Bindu");
     }
 
 }
