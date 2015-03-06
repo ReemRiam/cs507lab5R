c1835a6df5c28fc1fec6c8ff750bdac29209acbf
ReemRiam <ReemRiam@users.noreply.github.com>
2015-03-05 23:52:32 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
 	System.out.println("Software Engineering Lab5");
+		System.out.println("This course is teached by Prof.James");
         System.out.println("Reem, Riam, Bindu");
     }
 
