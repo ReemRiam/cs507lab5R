a079d79ebceef3c938c5d440e0e12c1a8c3e69ff
ReemRiam <ReemRiam@users.noreply.github.com>
2015-03-09 16:03:16 -0400

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 		System.out.println("This course is teached by Prof.James");
         System.out.println("Reem, Riam, Bindu");
         System.out.println("Hello lab5+6");
+        System.out.println("The code was edited by all group members");
 
     }
 
